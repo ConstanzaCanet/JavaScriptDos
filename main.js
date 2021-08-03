32b39702dcd9244a5eec03a8af8fa1899650bfd5
ConstanzaCarnet <carnetconstanza@gmail.com>
2021-08-03 19:26:13 -0300

    imprime bien apI commit

--- main.js
+++ main.js
@@ -46,15 +46,19 @@ let IMGBASE= 'https://image.tmdb.org/t/p/original'
       peli.forEach(e=>{
         let peliculas = document.getElementById('productos')
         peliculas.innerHTML +=
-        `<div class="card mb-3 carta col">
-          <img src="${IMGBASE}${e.poster_path}" class="card-img-top img-fluid" alt="${e.title}">
-          <div class="card-body">
-            <h5 class="card-title">${e.title}</h5>
-            <p class="card-text">${e.overview}</p>
-            <p class="card-text"><small class="text-muted">${e.genre_ids}</small></p>
-            <button class="btn btn-primary" type="" onclick="Adquirir()">Adquirir</button>
-         </div>
+ 
+        `<div class="col" id='cardd'>
+          <div class="card h-100">
+            <img src="${IMGBASE}${e.poster_path}" class="card-img-top" alt="${e.title}">
+            <div class="card-body"  id='card'>
+              <h5 class="card-title">${e.title}</h5>
+              <p class="card-text">${e.overview}</p>
+              <p class="card-text"><small class="text-muted">${e.release_date}</small></p>
+              <button class="btn btn-outline-primary btn-sm" type="" onclick="Adquirir()">Adquirir</button>
+            </div>
+          </div>
         </div>`
+        
       })
     }
 })
@@ -63,9 +67,14 @@ let IMGBASE= 'https://image.tmdb.org/t/p/original'
 //CREO UN SELECT QUE ME MUESTRE POR GENEROS
 function cambioOpcionesSelcet(){
   let genreId = document.getElementById('opciones2').value
-  let baseImagenCambiada = 'https://image.tmdb.org/t/p/w780'
   let URLGENRE = `https://api.themoviedb.org/3/discover/movie?api_key=${APIKEY}&language=en-US&page=1&primary_release_year=2019&with_genres=${genreId}`
+  
+  //aqui vacio el div que contiene resultados para imprimir todo en la misma pagina sin que me lo sume
 
+  document.getElementById('contenedor').innerHTML=''
+  document.getElementById('productos').innerHTML=''
+
+  //tomo la api y traigo datos, filtrando por generos con genreId
 
   $.get(URLGENRE,function(data,status) {
     console.log(data)
@@ -73,15 +82,18 @@ function cambioOpcionesSelcet(){
       let peli = data.results
       peli.forEach(e=> {
           document.getElementById('contenedor').innerHTML +=
-          `<div class="card mb-3 carta col">
-          <img src="${baseImagenCambiada}${e.backdrop_path}" class="card-img-top img-fluid" alt="${e.title}">
-          <div class="card-body">
-          <h5 class="card-title">${e.title}</h5>
-          <p class="card-text">${e.overview}</p>
-          <p class="card-text"><small class="text-muted">${e.genre_ids}</small></p>
-          <button class="btn btn-primary" type="" onclick="Adquirir()">Adquirir</button>
-           </div>
+          `<div class="col" id='cardd'>
+              <div class="card h-100">
+                 <img src="${IMGBASE}${e.poster_path}" class="card-img-top" alt="${e.title}">
+                  <div class="card-body"  id='card'>
+                     <h5 class="card-title">${e.title}</h5>
+                     <p class="card-text">${e.overview}</p>
+                     <p class="card-text"><small class="text-muted">${e.release_date}</small></p>
+                     <button class="btn btn-outline-primary btn-sm" type="" onclick="Adquirir()">Adquirir</button>
+                  </div>
+              </div>
           </div>`
+        
       })
     }
   })
@@ -91,56 +103,27 @@ function cambioOpcionesSelcet(){
 //FUNCION DE BUSQUEDA Y OFRECIMIENTO--
 //ME ENCUENTRO TRATANDO DE FUSIONAR ESTA FUNCION CON LA API, AUN ME ENCUENTRO LEYENDO
 //LA DOCUMENTACION DE LA API PARA APLICARLA CORRECTAMENTE
-function searchName() {
-  let what = document.getElementById("busca").value
-  if (Movies.find(Movie => Movie.title == what)) {
-    let thisMovie = (Movies.find(Movie => Movie.title == what))
-    console.log(`Tenemos esa peli! ${thisMovie.title}`);
-    let conte1 = document.getElementsByClassName('contenedor');
-     
-    let ress = document.createElement('article')
-    ress.innerHTML = 
-                `<div class="card m-5" style="width: 18rem;">
-                   <img src="${thisMovie.image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">${thisMovie.title}</h5>
-                     <p class="card-text"> ${thisMovie.gender} , posee una duracion de ${thisMovie.duration}, del año ${thisMovie.year}. Precio de compra ${thisMovie.fullprice}</p>
-                    </div>
-                </div>`
-  conte1[0].appendChild(ress);
-  } else if (what == 0) {
-    console.log(('No has colocado datos'))
-   
-    let pero = document.createElement('article')
-    pero.innerHTML = `No has colocado datos!`
-    document.body.appendChild(pero)
-    
-    return searchName()
-
-  }else{
-    let ups = document.createElement('article')
-    ups.innerHTML = `Lo sentimos no tenemos esa peli!`;
-    document.body.appendChild(ups)
-
-    return(`Lo sentimos no tenemos esa peli!`);
-  }
-      
-  let esp = document.createElement('br')
-  esp.innerHTML = `<br></br>`
-  document.body.appendChild(esp)
-
-}
-
-let tecla = document.getElementById("select")
-tecla.addEventListener("click", searchName)
 
 
+$(document).ready(function() {
+  var url = 'http://api.themoviedb.org/3/',
+  mode = 'search/movie',
+  input,
+  movieName,
+  APIKEY;
 
-
-
-let APINAMES = `http://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=king+lion`
-$.get(APINAMES, function(data,status) {
-  console.log(data)
-  console.log(status)
-
-})
+  $('#select').click(function() {
+      var input = $('#busca').val(),
+          movieName = encodeURI(input);
+      $.ajax({
+          url: url + mode + APIKEY + '&query='+movieName ,
+          dataType: 'jsonp',
+          success: function(data) {
+           console.log(data);
+          },
+          error: function (request, status, error) {
+           alert(status + ", " + error);
+          }
+      });
+  });
+});
