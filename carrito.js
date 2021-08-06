53b448a2f19568c06e758870a77b81a327f07cad
ConstanzaCarnet <carnetconstanza@gmail.com>
2021-08-06 18:15:51 -0300

    busca by title solved error apI commit

--- carrito.js
+++ carrito.js
@@ -10,11 +10,10 @@ class Carrito{
 
 //UTILIZO UNA FUNCION QUE ME PERMITA TRAER DE LA API UNA SOLA PELICULA, LA QUE YO ELIJA
 //PARA ELLO UTILIZO LA FUNCION DE 'BUSCAR POR ID' QUE POSEE LA API, COLOCO LA ID EN LA CARD IMPRESA Y LA BUSCO 
-var btnAdquirir = document.getElementById('#adquirir');
-$('#adquirir').on(click,adquirir)
 
-function adquirir(e) {
-    let movieId = e.target.classList[3]
+function adquirir() {
+    var btnAdquirir = document.getElementsByClassName('adquirir')
+    let movieId = btnAdquirir.this[3]
     let dataCompra = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${APIKEY}&language=en-US`
 
     $.get(dataCompra, function(data, status) {
@@ -25,4 +24,4 @@ function adquirir(e) {
 }
 
 
-
+adquirir()
